efbb21356a4f109db876d2415a5640c4f73998a4
thiag0-dot <thiagodeginofilho@gmail.com>
2022-11-09 15:04:58 -0300

    Primeiro commit

---
+++ ListaDeCompra/ListaDeCompra/View/Formulario.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListaDeCompra.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Formulario : ContentPage
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+    }
+}
