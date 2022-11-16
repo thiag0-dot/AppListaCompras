972289cdd06cd53adaad968b34d0ed5806383802
thiag0-dot <thiagodeginofilho@gmail.com>
2022-11-16 14:50:45 -0300

    Segundo commit - Model

---
+++ ListaDeCompra/ListaDeCompra/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ListaDeCompra.Model;
+using SQLite;
+
+
+namespace ListaDeCompra.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Nome = ?, Qnt = ?, Preco = ? WHERE id = ?";
+
+            return _conn.QueryAsync<Produto>(sql, p.Nome, p.Qnt, p.Preco, p.Id);
+        }
+    }   
+}
---
+++ ListaDeCompra/ListaDeCompra/Model/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ListaDeCompra.Model
+{
+    public class Produto
+    {
+        string _nome;
+        double _qnt;
+
+        [PrimaryKey, Autoincrement]
+        public int Id { get; set; }
+
+        public string Nome 
+        {
+            get => _nome;
+            set
+            {
+                if (value == null)
+                    throw new Exception("Nome inválido");
+
+                _nome = value;
+            }
+        }
+
+        public double Qnt
+        {
+            get
+            {
+                return _qnt;
+            }
+
+            set
+            {
+                if(!double.TryParse(value.ToString(), out _qnt))
+                    _qnt = 0.0;
+
+                if (value == 0)
+                    throw new Exception("Quantidade inválida");
+
+                _qnt = value;
+            }
+        }
+
+        public double preco { get; set; }
+
+        public double Precototal 
+        {
+            get => Preco * Qnt;
+        }
+    }
+}
